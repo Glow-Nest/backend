f32066f9493ced729aa0d4c7d4a40e7ec948ad04
iamsu <iamsuhani5@gmail.com>
2025-05-08 20:15:55 +0200

    product is created

---
+++ UnitTest/Features/ProductTest/CreateProductCommandTest.cs
@@ -0,0 +1,40 @@
+﻿using Application.AppEntry.Commands.Product;
+
+namespace UnitTest.Features.ProductTest;
+
+public class CreateProductCommandTest
+{
+    [Fact]
+    public void CreateProductCommand_ShouldReturnSuccess_WhenProductIsCreated()
+    {
+        // Arrange
+        var productName = "product1";
+        var productPrice = 100;
+        var productDescription = "description1";
+        var imageUrl = "imageUrl";
+        var inventoryCount = 10;
+
+        // Act
+        var command = CreateProductCommand.Create(productName, productPrice, productDescription, imageUrl, inventoryCount);
+
+        // Assert
+        Assert.True(command.IsSuccess);
+    }
+    
+    [Fact]
+    public void CreateProductCommand_ShouldReturnFailure_WhenProductNameIsEmpty()
+    {
+        // Arrange
+        var productName = "";
+        var productPrice = 100;
+        var productDescription = "description1";
+        var imageUrl = "imageUrl";
+        var inventoryCount = 10;
+
+        // Act
+        var command = CreateProductCommand.Create(productName, productPrice, productDescription, imageUrl, inventoryCount);
+
+        // Assert
+        Assert.False(command.IsSuccess);
+    }
+}
---
+++ UnitTest/Features/ProductTest/CreateProductHandlerTest.cs
@@ -0,0 +1,42 @@
+﻿using Application.AppEntry.Commands.Product;
+using Application.Handlers.ProductHandlers;
+using Domain.Aggregates.Product;
+using Domain.Common.OperationResult;
+using Moq;
+
+namespace UnitTest.Features.ProductTest;
+
+public class CreateProductHandlerTest
+{
+    private readonly Mock<IProductRepository> _mockProductRepository;
+    private readonly CreateProductHandler _handler;
+    
+    public CreateProductHandlerTest()
+    {
+        _mockProductRepository = new Mock<IProductRepository>();
+        _handler = new CreateProductHandler(_mockProductRepository.Object);
+    }
+    
+    [Fact]
+    public async Task HandleAsync_ShouldReturnSuccess_WhenProductIsCreated()
+    {
+        // Arrange
+        var productName = "product1";
+        var productPrice = 100;
+        var productDescription = "description1";
+        var imageUrl = "imageUrl";
+        var inventoryCount = 10;
+
+        var command = CreateProductCommand.Create(productName, productPrice, productDescription, imageUrl, inventoryCount);
+        
+        _mockProductRepository
+            .Setup(repo => repo.AddAsync(It.IsAny<Product>()))
+            .ReturnsAsync(Result.Success());
+        
+        // Act
+        var result = await _handler.HandleAsync(command.Data);
+        
+        // Assert
+        Assert.True(result.IsSuccess);
+    }
+}
