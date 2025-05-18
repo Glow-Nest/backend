c8389a5fc091dc57580014d1e72b3a39eff1d7de
rajipkanxo01 <pdlrajip12@gmail.com>
2025-05-18 17:26:02 +0200

    Mark order as ready for pick up done

--- src/Core/Application/Extensions/ApplicationExtensions.cs
+++ src/Core/Application/Extensions/ApplicationExtensions.cs
@@ -84,6 +84,8 @@ public static class ApplicationExtensions
         serviceCollection.AddScoped<ICommandHandler<CreateOrderCommand, OrderId>, CreateOrderHandler>();
         serviceCollection.AddScoped<ICommandHandler<UpdateOrderItemsCommand, OrderId>, UpdateOrderItemCommandHandler>();
         serviceCollection.AddScoped<ICommandHandler<UpdatePickupDateCommand, OrderId>, UpdatePickupDateCommandHandler>();
+        serviceCollection.AddScoped<ICommandHandler<MarkOrderAsCompletedCommand, None>, MarkOrderAsCompletedCommandHandler>();
+        serviceCollection.AddScoped<ICommandHandler<MarkOrderAsReadyForPickupCommand, None>, MarkOrderAsReadyForPickupCommandHandler>();
         
         // payment
         serviceCollection.AddScoped<ICommandHandler<CreateCheckoutSessionCommand, string>, CreateCheckoutSessionCommandHandler>();
--- src/Core/Application/Handlers/OrderHandlers/MarkOrderAsReadyForPickupCommandHandler.cs
+++ src/Core/Application/Handlers/OrderHandlers/MarkOrderAsReadyForPickupCommandHandler.cs
@@ -1,13 +1,24 @@
 using Application.AppEntry;
 using Application.AppEntry.Commands.Order;
+using Domain.Aggregates.Order;
 using OperationResult;
 
 namespace Application.Handlers.OrderHandlers;
 
-public class MarkOrderAsReadyForPickupCommandHandler : ICommandHandler<MarkOrderAsReadyForPickupCommand, None>
+public class MarkOrderAsReadyForPickupCommandHandler(IOrderRepository orderRepository) : ICommandHandler<MarkOrderAsReadyForPickupCommand, None>
 {
-    public Task<Result<None>> HandleAsync(MarkOrderAsReadyForPickupCommand command)
+    private readonly IOrderRepository _orderRepository = orderRepository;
+    public async Task<Result<None>> HandleAsync(MarkOrderAsReadyForPickupCommand command)
     {
-        throw new NotImplementedException();
+        var orderResult = await _orderRepository.GetAsync(command.OrderId);
+        if (!orderResult.IsSuccess)
+        {
+            return Result<None>.Fail(orderResult.Errors);
+        }
+
+        var order = orderResult.Data;
+        order.MarkAsReadyForPickup();
+
+        return Result<None>.Success(None.Value);
     }
 }
