04e2dda8f66fe7090236b48d89a465328090cdf1
iamsu <iamsuhani5@gmail.com>
2025-05-23 17:04:49 +0200

    change in handler class

--- src/Infrastructure/EfcQueries/Queries/Category/GetAllCategoryWithServiceQueryHandler.cs
+++ src/Infrastructure/EfcQueries/Queries/Category/GetAllCategoryWithServiceQueryHandler.cs
@@ -1,36 +1,36 @@
-﻿using Domain.Aggregates.ServiceCategory;
+﻿using EfcQueries.Models;
+using Microsoft.EntityFrameworkCore;
 using OperationResult;
 using QueryContracts.Contracts;
 using QueryContracts.Queries.Service;
 
 namespace EfcQueries.Queries.Category;
 
-public class GetAllCategoryWithServiceQueryHandler (ICategoryRepository categoryRepository) : IQueryHandler<GetAllCategoriesWithServices.Query,Result<GetAllCategoriesWithServices.Answer>>
+public class GetAllCategoryWithServiceQueryHandler(PostgresContext context)
+    : IQueryHandler<GetAllCategoriesWithServices.Query, Result<GetAllCategoriesWithServices.Answer>>
 {
+    private readonly PostgresContext _context = context;
 
     public async Task<Result<GetAllCategoriesWithServices.Answer>> HandleAsync(GetAllCategoriesWithServices.Query query)
     {
-        var categoriesResult = await categoryRepository.GetCategoriesWithServicesAsync();
-        
-        if (!categoriesResult.IsSuccess)
-        {
-            return Result<GetAllCategoriesWithServices.Answer>.Fail(categoriesResult.Errors);
-        }
-        
-        var categories = categoriesResult.Data;
+        var categories = await _context.Categories
+            .Include(c => c.MediaUrls)
+            .Include(c => c.Services)
+            .ToListAsync();
+
         var categoryDtos = categories.Select(c => new GetAllCategoriesWithServices.CategoryWithServicesDto(
-            c.CategoryId.Value.ToString(),
-            c.CategoryName.Value,
-            c.Description.Value,
-            c.MediaUrls.Select(m => m.Value).ToList(),
+            c.CategoryId.ToString(),
+            c.CategoryName,
+            c.Description,
+            c.MediaUrls.Select(m => m.Url).ToList(),
             c.Services.Select(s => new GetAllCategoriesWithServices.ServiceDto(
-                s.ServiceId.Value.ToString(),
-                s.Name.Value,
-                s.Price.Value,
+                s.ServiceId.ToString(),
+                s.Name,
+                s.Price,
                 s.Duration.ToString()
             )).ToList()
         )).ToList();
-        
+
         return Result<GetAllCategoriesWithServices.Answer>.Success(new GetAllCategoriesWithServices.Answer(categoryDtos));
     }
 }
